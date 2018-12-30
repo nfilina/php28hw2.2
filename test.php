df1e0a68dd70b1f568c08419267ab12a4e164353
nfilina <45875694+nfilina@users.noreply.github.com>
2018-12-30 18:29:20 +0700

    Update test.php

--- test.php
+++ test.php
@@ -13,33 +13,30 @@ if (isset($_POST['check-test'])) {
                 echo 'Должны быть решены все задания!';
                 exit;
             }
-        }
 
         $i = 0;
-        $questions = 0;
-        foreach ($testFile as $key => $item) {
+        $questions = 0;            	
             $questions++;
             // Здесь идет определение названия класса для блока с вопросом и ответом, чтобы выводить красный/зеленый фон для удобства
             // А также прибавляется 1 к переменной $i, если ответ правильный
             if ($item['correct_answer'] === $_POST['answer' . $key]) {
                 $i++;
                 $infoStyle = 'correct';
-            } else {
+            } else {    //
                 $infoStyle = 'incorrect';
             }
-
+            
             // Вывод блока с вопросом и ответом
-            echo "<div>";
+            echo "<pre>";
             echo 'Вопрос: ' . $item['question'] . '<br>';
             echo 'Ваш ответ: ' . $item['answers'][$_POST['answer' . $key]] . '<br>';
             echo 'Правильный ответ: ' . $item['answers'][$item['correct_answer']] . '<br>';
-            echo '</div>';
+            echo '</pre>';
             echo '<hr>';
         }
         echo '<p style="font-weight: bold;">Итого правильных ответов: ' . $i . ' из ' . $questions . '</p>';
     }
 }
-
 
 ?>
 
@@ -53,30 +50,31 @@ if (isset($_POST['check-test'])) {
 
  <!-- Если пользователь находиться на тесте, ссылка введет на страницу с тестами -->
 
-    <a href="<?php echo isset($_POST['check-test']) ? $_SERVER['HTTP_REFERER'] : 'list.php' ?>"><div><b>На страницу со списком тестов</b>>></div></a><br>
+    <a href="<?php echo isset($_POST['check-test']) ? $_SERVER['HTTP_REFERER'] : 'list.php' ?>"><b>Вернуться к списку тестов</b>  >></a><br>
 
-    <hr>
-    
+    <hr>    
 
 <form method="POST">
 
     <h1><?php echo basename($allTests[$number]); ?></h1>
-            <?php foreach($test as $key => $item): ?>       
-
+    <?php foreach($test as $key => $item): ?>  
+                
     <fieldset>
+
         <legend><?php echo $item['question'] ?></legend>
-        
-            <label><input type="radio" name="answer<?php echo $key ?>" value="0"><?php echo $item['answers'][0]?></label>
-            <label><input type="radio" name="answer<?php echo $key ?>" value="1"><?php echo $item['answers'][1]?></label>
-            <label><input type="radio" name="answer<?php echo $key ?>" value="2"><?php echo $item['answers'][2]?></label>
-            <label><input type="radio" name="answer<?php echo $key ?>" value="3"><?php echo $item['answers'][3]?></label>
-        
+        <?php foreach($item['answers'] as $keys => $itemM): ?>           
+
+            <label><input type="radio" name="answer<?php echo $key ?>" value="<?php echo $keys ?>"><?php echo $itemM ?></label><br>
+           
+        <?php endforeach; ?>
+
     </fieldset>
 
-    <?php endforeach; ?>
-    <input type="submit" name="check-test" value="Проверить">
-</form>
+    <?php endforeach; ?><br>
 
+
+    <input type="submit" name="check-test" value="Проверить">
+</form><br>
 
 
     <!-- Нажал на кнопку проверки теста -  вывели ему результаты -->
